1795a9c9523913a7e224661e2f91a363d31722d7
Mayorov Evgeny <motormen@gmail.com>
2016-04-19 18:55:38 +0300

    Added composite ws app

--- ws_admin/app.py
+++ ws_admin/app.py
@@ -1,6 +1,6 @@
-import ikcms.ws_apps.base
+import ikcms.ws_apps.composite
 
-class App(ikcms.ws_apps.base.App):
+class App(ikcms.ws_apps.composite.App):
 
     def get_handlers(self):
         from .handlers import handlers
