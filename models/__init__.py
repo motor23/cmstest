490f92b398b110d4009dff66d124aed8fc2251d2
Aleksandr Osipov <osipov.as89@gmail.com>
2016-06-06 17:23:58 +0400

    Add User model

--- models/__init__.py
+++ models/__init__.py
@@ -1,39 +1,39 @@
 from sqlalchemy.schema import MetaData
-from sqlalchemy import (
-    Column,
-    Integer,
-    DateTime,
-    String,
-    Text,
-    Enum,
-    Boolean,
-    ForeignKey,
-)
-from sqlalchemy.orm import (
-    relation,
-)
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from iktomi.db.sqla.declarative import AutoTableNameMeta, TableArgsMeta
+from iktomi.db.sqla.declarative import AutoTableNameMeta
+from iktomi.db.sqla.declarative import TableArgsMeta
 
-
-metadata = MetaData()
 
 TABLE_ARGS = {
     'mysql_engine': 'InnoDB',
     'mysql_default charset': 'utf8',
 }
 
-class ModelsMeta(AutoTableNameMeta, TableArgsMeta(TABLE_ARGS)): pass
 
-Base = declarative_base(
-    metadata=metadata,
-    name='Base',
-    metaclass=ModelsMeta,
-)
+metadata = MetaData()
+
+
+class ModelsMeta(AutoTableNameMeta, TableArgsMeta(TABLE_ARGS)):
+    pass
+
+
+Base = declarative_base(metadata=metadata, name='Base', metaclass=ModelsMeta)
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True)
+    login = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=True)
 
 
 class Tag(Base):
-
     id = Column(Integer, primary_key=True)
     title = Column(String(500), default='')
 
@@ -49,9 +49,6 @@ class Doc_Tag(Base):
 
 
 class Doc(Base):
-
     id = Column(Integer, primary_key=True)
     title = Column(String(500), default='')
-    tags = relation(Tag, secondary=Doc_Tag.__table__)
-
-
+    tags = relationship(Tag, secondary=Doc_Tag.__table__)
