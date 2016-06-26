64e03da20466da5eeb1b165f6178834b16e69f64
Mayorov Evgeny <motormen@gmail.com>
2016-06-26 15:41:31 +0300

    Modify aioredis component

--- tests/ikcms/ws_components/cache/test_aioredis.py
+++ tests/ikcms/ws_components/cache/test_aioredis.py
@@ -1,30 +1,30 @@
 import asyncio
 from unittest import TestCase
 from unittest.mock import MagicMock
-from ikcms.ws_components.cache.aioredis import Cache
+from ikcms.ws_components.cache.aioredis import component
 
 class AIORedisTestCase(TestCase):
 
     def test_cache(self):
         app = MagicMock()
+        del app.cache
         app.cfg.REDIS_HOST = 'localhost'
         app.cfg.REDIS_PORT = 6379
 
-        @asyncio.coroutine
-        def run():
-            cache = yield from Cache.create(app)
+        async def run():
+            cache = await component().create(app)
 
-            yield from cache.delete(b'test_key')
+            await cache.delete(b'test_key')
 
-            value = yield from cache.get(b'test_key')
+            value = await cache.get(b'test_key')
             self.assertIsNone(value)
 
-            yield from cache.set(b'test_key', b'test_value')
-            value = yield from cache.get(b'test_key')
+            await cache.set(b'test_key', b'test_value')
+            value = await cache.get(b'test_key')
             self.assertEqual(value, b'test_value')
 
-            yield from cache.delete(b'test_key')
-            value = yield from cache.get(b'test_key')
+            await cache.delete(b'test_key')
+            value = await cache.get(b'test_key')
             self.assertIsNone(value)
 
         asyncio.get_event_loop().run_until_complete(run())
--- ws_admin/app.py
+++ ws_admin/app.py
@@ -1,5 +1,7 @@
 import ikcms.ws_apps.composite
-import ikcms.ws_components.db
+import ikcms.ws_components.db.sqla
+import ikcms.ws_components.cache.aioredis
+
 from . import components
 
 from .menu import menu
@@ -8,7 +10,9 @@ from .streams import streams
 class App(ikcms.ws_apps.composite.App):
 
     components = [
-        ikcms.ws_components.db.sqla(),
+        ikcms.ws_components.db.sqla.component(),
+        ikcms.ws_components.cache.aioredis.component(),
+
         components.auth(),
         components.cinfo(),
         components.menu(menu=menu),
