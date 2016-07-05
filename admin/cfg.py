f83a4206fbea62588049efa358858fb31f92275d
Aleksandr Osipov <osipov.as89@gmail.com>
2016-07-05 16:52:12 +0400

    Add admin:serve running both HTTP and WS for development use

--- admin/cfg.py
+++ admin/cfg.py
@@ -2,10 +2,14 @@ import ikcms.apps.web
 
 
 class Cfg(ikcms.apps.web.Cfg):
-
     SITE_ID = 'admin'
-    WS_SERVER_URL = 'ws://localhost:8888/ws'
+
+    HTTP_SERVER_HOST = 'localhost'
+    HTTP_SERVER_PORT = 8000
+
+    WS_SERVER_HOST = 'localhost'
+    WS_SERVER_PORT = 8888
 
     DATABASES = {'main': 'mysql://root@localhost/web?charset=utf8'}
-#    DATABASES = {'': 'aiopg://root@localhost/web?charset=utf8'}
+
     LOG_LEVEL = 'DEBUG'
